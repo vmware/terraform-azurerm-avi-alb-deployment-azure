215e3cd968d26314f1299e2b2d33113d68976b79
Sean Larimore <slarimore02@gmail.com>
2022-08-09 23:01:27 +0000

    disable prevent_deletion_if_contains_resources

--- main.tf
+++ main.tf
@@ -20,7 +20,11 @@ terraform {
   }
 }
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
   skip_provider_registration = "true"
 }
 resource "azurerm_resource_group" "avi" {
